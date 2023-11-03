ae536b2b0369b3bc9f95981b12a0010a7e667983
Sulaiman Shuja <Sulaimanshuja@gmail.com>
2023-11-03 18:09:39 +0500

    file updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo2 from './logo2.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <h3>
           Waste Management System
         </h3>
