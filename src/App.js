89585c31a8e0645eca75b5ef9c7e261f51e885c9
Sulaiman Shuja <Sulaimanshuja@gmail.com>
2023-11-03 17:56:40 +0500

    Updated Version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo1 from './logo1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo1} className="App-logo" alt="logo" />
+        <h3>
+          Waste Management System
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
